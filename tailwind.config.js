22cedfaddffb04fabebbe55522215bf201f9a17a
ClaudioLenci <73424774+ClaudioLenci@users.noreply.github.com>
2024-12-19 16:38:38 +0100

    Add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ export default {
     extend: {
       fontFamily: {
         Noto: ["Noto Sans", "serif"]
+      },
+      colors: {
+        "primary-charcoal": "#373F51",
+        "primary-moonstone": "#58A4B0",
+        "secondary-green": "#A7C957",
+        "secondary-coral": "#E07A5F",
+        "neutral-white": "#D8DBE2"
       }
     },
   },
