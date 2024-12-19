6365361f59a502fdb2420ef994e6e8f60eb2a558
ClaudioLenci <73424774+ClaudioLenci@users.noreply.github.com>
2024-12-19 17:56:42 +0100

    Update navbar + Add logo

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,22 @@
 import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
 
 export function Navbar() {
     const roots: Array<{page: string, path: string}> = [
         {page: "Cerca aziende", path: "/search"}
     ]
     return (
-        <div className="bg-primary-moonstone text-black font-medium p-4 flex flex-row items-center justify-between">
-            {roots.map((root, index) => (
-                <Link className="" to={root.path} key={index}>{root.page}</Link>
-            ))}
-            <Link to="/login" className="bg-primary-charcoal text-neutral-white rounded-xl py-2 px-4 self-end">Login</Link>
+        <div className="bg-secondary-green font-medium gap-6 px-4 flex flex-row">
+            <Link to="/" className="flex flex-row items-center gap-3">
+                <img src={logo} className="h-12"/>
+                <h1 className="font-semibold text-2xl text-black">CareerLink</h1>
+            </Link>
+            <div className="flex flex-row items-center justify-between w-full py-4">
+                {roots.map((root, index) => (
+                    <Link className="text-primary-charcoal" to={root.path} key={index}>{root.page}</Link>
+                ))}
+                <Link to="/login" className="bg-primary-charcoal text-neutral-white rounded-xl py-2 px-4 self-end">Login</Link>
+            </div>
         </div>
     );
 }
