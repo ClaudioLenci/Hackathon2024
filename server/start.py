d6d59bb0f0eb7f08dc462c560be9f4a89be2ec93
Marcofire97 <mudricm1@gmail.com>
2024-12-20 02:18:16 +0100

    changed start.py

--- server/start.py
+++ server/start.py
@@ -2,23 +2,35 @@ import subprocess
 import sys
 import os
 
-def install_requirements():
-    if os.path.exists('requirements.txt'):
-        print("Trovato requirements.txt, installo le dipendenze...")
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--no-cache-dir', '-r', 'requirements.txt'])
+def create_venv():
+    """Crea un ambiente virtuale se non esiste."""
+    if not os.path.exists('./venv'):
+        print("Creazione dell'ambiente virtuale...")
+        subprocess.check_call([sys.executable, '-m', 'venv', 'venv'])
     else:
+        print("Ambiente virtuale trovato.")
+
+def install_requirements():
+    """Installa i requisiti nell'ambiente virtuale."""
+    print("Attivando l'ambiente virtuale...")
+    venv_python = os.path.join('venv', 'bin', 'python') if os.name != 'nt' else os.path.join('venv', 'Scripts', 'python.exe')
+    if not os.path.exists('requirements.txt'):
         print("Nessun file requirements.txt trovato.")
         sys.exit(1)
+    print("Trovato requirements.txt, installo le dipendenze...")
+    subprocess.check_call([venv_python, '-m', 'pip', 'install', '--no-cache-dir', '-r', 'requirements.txt'])
 
 def run_app():
+    """Esegue l'applicazione."""
+    venv_python = os.path.join('venv', 'bin', 'python') if os.name != 'nt' else os.path.join('venv', 'Scripts', 'python.exe')
     if os.path.exists('app.py'):
         print("Eseguendo app.py...")
-        subprocess.check_call([sys.executable, 'app.py'])
+        subprocess.check_call([venv_python, 'app.py'])
     else:
         print("Nessun file app.py trovato.")
         sys.exit(1)
-        
 
 if __name__ == '__main__':
+    create_venv()
     install_requirements()
     run_app()
